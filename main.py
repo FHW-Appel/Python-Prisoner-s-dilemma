3143fb58dfa6e24fde13d80eb2277bea130771c2
Sönke Appel <appel@fh-westkueste.de>
2024-08-16 14:07:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from mypackage.simulation import PPDSimulation
 def main():
     ppdsim = PPDSimulation()
     ppdsim.runsimtest()
+    candidates = ppdsim.initcandidates()
 
 if __name__ == "__main__":
     main()
