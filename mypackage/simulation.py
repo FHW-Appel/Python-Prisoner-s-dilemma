53648afa90ecc1c05d51c723a16c1a8153954e28
Klabeuterman <167600959+Klabeuterman@users.noreply.github.com>
2024-08-21 20:50:30 +0200

    Punkte werden zusammen gezählt

--- mypackage/simulation.py
+++ mypackage/simulation.py
@@ -3,7 +3,7 @@ from .ruleset import Ruleset
 from .basestrategy import Strategy
 from .default_strategies.defaultstrat import * # importiert alle Klassen des Files "defaultstrat"
 from .custom_strategies import * # Hier muss noch ein Befehl gefunden werden mit dem alle Klassen eines Ordners eingebunden werden können.
-
+import pandas as pd
 
 class PPDSimulation:
 
@@ -30,23 +30,33 @@ class PPDSimulation:
     def runsim(self):
         rule = Ruleset()
         candidates = self.initcandidates()
-        for candidate1 in candidates[:-1]: # Schleife über alle Kandidaten bis auf den letzten Kandidaten
-            for candidate2 in candidates[candidates.index(candidate1)+1:]: # Schleife ab candidate1 + 1
-                print("Paarung: " + candidate1.name + "   vs.   " + candidate2.name)
+        num_candidates = len(candidates)
+        #print(candidates.name)
+        sim_results = pd.DataFrame({"Startegie Objekt": candidates,
+                                    "Total Points": [0] * num_candidates,
+                                    "Average Points": [0] * num_candidates})
+        print(sim_results)
+        for i1 in range(num_candidates-1): # Schleife über alle Kandidaten bis auf den letzten Kandidaten
+            for i2 in range(i1+1, num_candidates): # Schleife ab candidate1 + 1
+                print("Paarung: " + candidates[i1].name + "   vs.   " + candidates[i2].name)
                 for rep in range(rule.repetitions):
                     histc1 = []
                     histc2 = []
                     pointsc1 = 0
                     pointsc2 = 0
                     for turn in range(rule.turns):
-                        histc1.append(candidate1.react(turn, histc1, histc2))
-                        histc2.append(candidate2.react(turn, histc2, histc1))
+                        histc1.append(candidates[i1].react(turn, histc1, histc2))
+                        histc2.append(candidates[i2].react(turn, histc2, histc1))
                         [pointsc1, pointsc2] = rule.evaluate_points(histc1[turn], histc2[turn], pointsc1, pointsc2)
-                        # Punkte in Pandas zählen
+                    sim_results["Total Points"].iloc[i1] += pointsc1
+                    sim_results["Total Points"].iloc[i2] += pointsc2
+                    # Punkte in Pandas zählen
                     print(pointsc1)
                     print(histc1)
                     print(histc2)
                     print(pointsc2)
+        sim_results["Average Points"] = sim_results["Total Points"] / num_candidates / rule.repetitions
+        print(sim_results)
     
     def initcandidates(self):
         listOfStrategies = Strategy.__subclasses__() # Ereugt eine Liste aller Klassen, die von Strategy abgeleitet wurden 
