4c6c0334fd7b66a203a6a56e57522bbe14fda39f
FHW-Appel <appel@fh-westkueste.de>
2025-03-05 09:33:31 +0100

    test wieder auskommentiert

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from mypackage.simulation import PPDSimulation
 def main():
     ppdsim = PPDSimulation()
     #ppdsim.runsimtest()
-    #candidates = ppdsim.initcandidates()    
-    #ppdsim.runsim()
-    ppdsim.runsimtest2()
+    #candidates = ppdsim.initcandidates()
+    #ppdsim.runsimtest2()   
+    ppdsim.runsim()
+    
 
 if __name__ == "__main__":
     main()
